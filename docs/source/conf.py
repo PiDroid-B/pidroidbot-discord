06644e97176e15e62bcf3072d51481b6928965c0
PiDroid-B Action New release <pidroid-b@users.noreply.github.com>
2021-04-02 19:13:15 +0000

    [SKIP] version bump v0.0.17 -> v0.0.18

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ author = "PiDroid-B"
 version = ""
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.17"
+release = "v0.0.18"
 
 
 # -- General configuration ---------------------------------------------------
