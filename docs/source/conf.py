d317f6b43705eee4d81c9452ab905133d7d428d5
PiDroid-B Action New release <pidroid-b@users.noreply.github.com>
2021-04-03 18:25:50 +0000

    [SKIP] version bump v0.0.23 -> v0.0.24

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = "PiDroid-B"
 version = ""
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.23"
+release = "v0.0.24"
 
 
 # -- General configuration ---------------------------------------------------
