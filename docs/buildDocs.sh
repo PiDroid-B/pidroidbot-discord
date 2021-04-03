ee8a08b2d2ef125e7a38a27dce222094588fda5f
PiDroid-B <pidroid-b@users.noreply.github.com>
2021-04-03 15:40:27 +0200

    test ci 36 fix translation + version

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -82,14 +82,14 @@ for current_version in ${versions}; do
       sphinx-build -b html docs/source/ docs/_build/html/${current_language}/${current_version} -D language="${current_language}"
 
       # PDF #
-      sphinx-build -b rinoh docs/source/ docs/_build/rinoh -D language="${current_language}"
-      mkdir -p "${docroot}/${current_language}/${current_version}"
-      cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/${PRJ_NAME}-docs_${current_language}_${current_version}.pdf"
-
-      # EPUB #
-      sphinx-build -b epub docs/source/ docs/_build/epub -D language="${current_language}"
-      mkdir -p "${docroot}/${current_language}/${current_version}"
-      cp "docs/_build/epub/target.epub" "${docroot}/${current_language}/${current_version}/${PRJ_NAME}-docs_${current_language}_${current_version}.epub"
+#      sphinx-build -b rinoh docs/source/ docs/_build/rinoh -D language="${current_language}"
+#      mkdir -p "${docroot}/${current_language}/${current_version}"
+#      cp "docs/_build/rinoh/target.pdf" "${docroot}/${current_language}/${current_version}/${PRJ_NAME}-docs_${current_language}_${current_version}.pdf"
+#
+#      # EPUB #
+#      sphinx-build -b epub docs/source/ docs/_build/epub -D language="${current_language}"
+#      mkdir -p "${docroot}/${current_language}/${current_version}"
+#      cp "docs/_build/epub/target.epub" "${docroot}/${current_language}/${current_version}/${PRJ_NAME}-docs_${current_language}_${current_version}.epub"
 
       # copy the static assets produced by the above build into our docroot
       rsync -av "docs/_build/html/" "${docroot}/"
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,17 +11,7 @@
 import os
 import sys
 
-# SET CURRENT_VERSION
 from git import Repo
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-# At top on conf.py (with other import statements)
 from recommonmark.transform import AutoStructify
 
 sys.path.insert(0, os.path.abspath("../.."))
@@ -331,5 +321,4 @@ def setup(app):
     app.add_transform(AutoStructify)
 
 
-print(html_context["languages"])
-print(html_context["versions"])
+print("----- INFO - config.py :", html_context["languages"], html_context["versions"])
