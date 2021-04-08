d1fbdd82dc6f1a8540e2e22f9425886bbbfe518a
PiDroid-B <pidroid-b@users.noreply.github.com>
2021-04-08 15:27:39 +0200

    test ci 41.12 badge

--- docs/UpdateDocs.sh
+++ docs/UpdateDocs.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
-set -x
+#set -x
 
 export PYTHONPATH=./..
 
 sphinx-apidoc -o source/ "${PYTHONPATH}" -f -d 1
 make gettext
+sphinx-intl update -p build/gettext
 
 for d in $(ls -d locales/*/ ); do
   lang="$( basename ${d})"
-  sphinx-intl update -p build/gettext -l "$lang"
+  for f in $(ls -d "${d}"/*/*.po ); do
+    if ! grep -q "Language:" "${f}" ; then
+      awk '/Language-Team/ { print; print "\"Language: '${lang}'\\n\""; next }1' "${f}" > "${f}.tmp"
+      mv "${f}.tmp" "${f}"
+    fi
+  done
 done
+
+#set -x
+#
+#export PYTHONPATH=./..
+#
+#sphinx-apidoc -o source/ "${PYTHONPATH}" -f -d 1
+#make gettext
+#
+#for d in $(ls -d locales/*/ ); do
+#  lang="$( basename ${d})"
+#  sphinx-intl update -p build/gettext -l "$lang"
+#done
