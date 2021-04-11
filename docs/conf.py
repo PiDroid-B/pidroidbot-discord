06289f3c360503d2d86ff1a43a83eac72c240e7f
PiDroid-B <pidroid-b@users.noreply.github.com>
2021-04-11 15:37:18 +0200

    test CI 51.3 on main

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'PiDroid-B'
 version = ""
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.1'
+release = 'v0.0.40'
 
 
 # -- General configuration ---------------------------------------------------
