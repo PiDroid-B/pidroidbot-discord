eb405d95e366c292798a79e772ffafbd32d94533
PiDroid-B Action New release <pidroid-b@users.noreply.github.com>
2021-04-11 17:56:04 +0000

    [SKIP] version bump v0.0.55 -> v0.0.56

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'PiDroid-B'
 version = ""
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.55"
+release = "v0.0.56"
 
 
 # -- General configuration ---------------------------------------------------
