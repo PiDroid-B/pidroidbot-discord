7659feff634e59a76a7bca2085d98d1e1fd8d1fc
PiDroid-B Action New release <pidroid-b@users.noreply.github.com>
2021-04-11 14:36:12 +0000

    [SKIP] version bump v0.0.45 -> v0.0.46

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ author = 'PiDroid-B'
 version = ""
 
 # The full version, including alpha/beta/rc tags
-release = "v0.0.45"
+release = "v0.0.46"
 
 
 # -- General configuration ---------------------------------------------------
--- pidroidbot_discord/__init__.py
+++ pidroidbot_discord/__init__.py
@@ -1,3 +1,3 @@
 """Initial documentation of SampleProject."""
 
-__version__ = "0.0.45"
+__version__ = "0.0.46"
