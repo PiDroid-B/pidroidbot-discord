806470761618b246182bbd0e5338abc18f34a70a
PiDroid-B <pidroid-b@users.noreply.github.com>
2021-04-18 16:33:53 +0200

    test CI 54.4 on tox/doc

--- devtools/UpdateI18N.sh
+++ devtools/UpdateI18N.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-set -x
-
-dir_before=`mktemp -d`
-dir_after=`mktemp -d`
-
-mkdir -p "$dir_before"/locales/
-rsync -vr -f '+ *.po' -f '+ **/' -f '- *' --prune-empty-dirs locales/. "$dir_before"/locales/
-mkdir -p "$dir_before"/docs/locales/
-rsync -vr -f '+ *.po' -f '+ **/' -f '- *' --prune-empty-dirs docs/locales/. "$dir_before"/docs/locales/
-
-pushd devtools/
-bash UpdateI18N.sh
-popd
-
-mkdir -p "$dir_after"/locales/
-rsync -vr -f '+ *.po' -f '+ **/' -f '- *' --prune-empty-dirs locales/. "$dir_after"/locales/
-mkdir -p "$dir_after"/docs/locales/
-rsync -vr -f '+ *.po' -f '+ **/' -f '- *' --prune-empty-dirs docs/locales/. "$dir_after"/docs/locales/
-
-git diff --check $dir_before/ $dir_after/ || {
-  echo "i18n require an update, please run devtools/updateI18N.sh"
-  exit 1
-}
-(environment) root@8e032461f1b7:/tmp/pidroidbot-discord# cat devtools/UpdateI18N.sh
-#!/bin/bash
 
 [ "$(dirname "$0")" != "." ] && {
   echo "Please go to the devtools' directory before run this script"
